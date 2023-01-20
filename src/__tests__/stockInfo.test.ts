8d315f0fbd2615df4aeb4450c5e88beb9726c3da
Ranjith Jagadeesh <86655894+ranjith-jagadeesh@users.noreply.github.com>
2023-01-20 10:21:50 +0530

    add test case

--- src/__tests__/stockInfo.test.ts
+++ src/__tests__/stockInfo.test.ts
@@ -1,19 +1,27 @@
 import { getStockInfo } from "../stockInfo";
 
 describe("Stock Info", () => {
-  it("Get Stock Info - Stock present in stock.json",  () => {
+  it("Get Stock Info - Stock present in stock.json", () => {
     const sku = "SXV420098/71/68";
     const res = getStockInfo(sku);
 
     expect(res.sku).toEqual(sku);
     expect(res.qty).toEqual(706);
   });
-    
-    it("Get Stock Info - Stock not present in stock.json", () => {
-        const sku = "KSS894454/75/76";
-        const res = getStockInfo(sku);
 
-        expect(res.sku).toEqual(sku);
-        expect(res.qty).toEqual(-85);
-    })
+  it("Get Stock Info - Verify Deep copy of Objects", () => {
+    const sku = "SXV420098/71/68";
+    const res = getStockInfo(sku);
+
+    expect(res.sku).toEqual(sku);
+    expect(res.qty).toEqual(706);
+  });
+
+  it("Get Stock Info - Stock not present in stock.json", () => {
+    const sku = "KSS894454/75/76";
+    const res = getStockInfo(sku);
+
+    expect(res.sku).toEqual(sku);
+    expect(res.qty).toEqual(-85);
+  });
 });
